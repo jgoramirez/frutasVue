a1f2ddb4bac75617043f478706c770d9ecb82196
Jose Guadalupe Omar Ramirez Mata <Jose.Ramirez@municipiodequeretaro.gob.mx>
2019-09-24 16:58:02 -0500

    Video 17

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <Cabecera></Cabecera>
+    <Lista></Lista>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cabecera from './components/Cabecera'
+import Lista from './components/Lista'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cabecera,
+    Lista
   }
 }
 </script>
